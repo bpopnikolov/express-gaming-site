97bba961af1e81df57c50cff26fbca779ea9faaa
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-03-19 16:08:08 +0200

    created http server and project structure

---
+++ app/app.js
@@ -0,0 +1,26 @@
+/* globals __dirname */
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+// route: public/js/app.js
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,69 @@
+/* globals process */
+const app = require('./app/app');
+const debug = require('debug')('http');
+const http = require('http');
+
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+};
+
+/* eslint-disable */
+const port = normalizePort(process.env.PORT || '3001');
+/* eslint-enable */
+
+const onError = (err) => {
+    if (err.syscall !== 'listen') {
+        throw err;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (err.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated priviliges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw err;
+    }
+};
+
+const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ?
+        'Pipe ' + addr :
+        'Port ' + addr.port;
+    debug('Listening on ' + bind);
+};
+
+if (debug.enabled) {
+   console.log('DEBUG');
+}
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
