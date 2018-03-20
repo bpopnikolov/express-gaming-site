1cfc7e3cad76874945dc828ee0318b9310ba1308
Rusi Rusev <roussy.roussev@gmail.com>
2018-03-20 13:42:38 +0200

    auth routers and pug files incomplete

---
+++ app/routes/auth/auth.routes.js
@@ -0,0 +1,20 @@
+const {
+    Router,
+} = require('express');
+
+const init = (app, data) => {
+    const router = new Router();
+    router
+        .get('/register', async (req, res) => {
+            res.render('/auth/register');
+        })
+        .get('/login', async (req, res) => {
+            res.render('/auth/login');
+        });
+
+    app.use('/superheroes', router);
+};
+
+module.exports = {
+    init,
+};
