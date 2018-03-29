37b5b0840e901e138d6859aa35fadc5a72084cfe
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-03-29 17:01:19 +0300

    added mdi icons css and fonts, side-nav refactoring

--- public/assets/js/core/nav/nav.js
+++ public/assets/js/core/nav/nav.js
@@ -6,7 +6,7 @@ $(function() {
         hover: false, // Activate on hover
         gutter: 0, // Spacing from edge
         belowOrigin: true, // Displays dropdown below the button
-        alignment: 'left', // Displays dropdown with edge aligned to the left of button
-        stopPropagation: false, // Stops event propagation
+        alignment: "left", // Displays dropdown with edge aligned to the left of button
+        stopPropagation: false // Stops event propagation
     });
 });
