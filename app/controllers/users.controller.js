58a4ca695fdc94cf6e62c03281cdfaf44f7b448e
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-03-31 16:22:44 +0300

    user profile changes

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,19 +1,5 @@
 const dbWrapper = require('../database-wrapper');
 
-const getUserIdByEmail = async (req, res, next) => {
-    const gameName = req.params.gameName;
-    const gameObj = await dbWrapper.games.getByName(gameName);
-
-    if (!gameObj) {
-        res.render('app/pageNotFound');
-    }
-
-    const context = {
-        gameObj,
-    };
-
-    res.render('app/games', context);
-};
 
 module.exports = {
 
